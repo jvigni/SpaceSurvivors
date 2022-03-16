598df200a3c6c11d19e42d0e6990569a109f3044
Juan <juan.vigni@gmail.com>
2022-03-16 14:37:49 -0300

    Working 1 !

--- Assets/Stage/Stages.cs
+++ Assets/Stage/Stages.cs
@@ -3,12 +3,15 @@
     public static Stage Stage1()
     {
         return new Stage("Stage I",
-            new IncreaseSpawnerAction(0, EnemyId.Alien1, 3),
-            new IncreaseSpawnerAction(.5f, EnemyId.Alien1, 5),
-            new IncreaseSpawnerAction(1, EnemyId.Alien1, 10),
+            new IncreaseSpawnerAction(0, EnemyId.Alien1, 7),
+            new IncreaseSpawnerAction(1, EnemyId.Alien1, 15),
+            new IncreaseSpawnerAction(2, EnemyId.Alien1, 30),
+            new IncreaseSpawnerAction(3, EnemyId.Alien1, 55),
+            new IncreaseSpawnerAction(4, EnemyId.Alien1, 100),
+            new IncreaseSpawnerAction(4, EnemyId.Alien1, 200),
 
-            new IncreaseSpawnerAction(0f, EnemyId.Asteroid, 2),
-            new IncreaseSpawnerAction(0.3f, EnemyId.Asteroid, 4),
-            new IncreaseSpawnerAction(2, EnemyId.Asteroid, 8));
+            new IncreaseSpawnerAction(1f, EnemyId.Asteroid, 2),
+            new IncreaseSpawnerAction(2f, EnemyId.Asteroid, 4),
+            new IncreaseSpawnerAction(3, EnemyId.Asteroid, 10));
     }
 }
