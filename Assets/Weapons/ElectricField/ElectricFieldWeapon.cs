cbf6453dd6cbcb88f2841f4da8da37afd8b38f30
Juan <juan.vigni@gmail.com>
2022-03-29 19:06:12 -0300

    ElectricField done

--- Assets/Weapons/ElectricField/ElectricFieldWeapon.cs
+++ Assets/Weapons/ElectricField/ElectricFieldWeapon.cs
@@ -7,13 +7,20 @@ public class ElectricFieldWeapon : Weapon
     [SerializeField] LayerMask targetLayer;
     [SerializeField] float radius;
     [SerializeField] int damage;
+    [SerializeField] int lv2DmgIncrease;
+    [SerializeField] float lv3RadiusIncrease;
+    [SerializeField] float lv4CooldownRed;
+
     ParticleSystem ps;
 
     public override WeaponID ID => WeaponID.ElectricField;
 
     protected override WeaponLevelData[] levelsData => new WeaponLevelData[]
     {
-        new WeaponLevelData("WEAPON: Electric Field I", "Electrify nearby enemies")
+        new WeaponLevelData("WEAPON: Electric Field I", "Electrify nearby enemies"),
+        new WeaponLevelData("Electric Field II", "Increased damage"),
+        new WeaponLevelData("Electric Field III", "Increased radius"),
+        new WeaponLevelData("Electric Field IV", "Faster shocks")
     };
 
     public override void OnTurnOn()
@@ -35,5 +42,21 @@ public class ElectricFieldWeapon : Weapon
             collider.GetComponent<Lifeform>().ReceiveDamage(damage);
 
         yield return null;
+    }
+
+    protected override void DoOnLevelUp(int level)
+    {
+        if (level == 2)
+            damage += lv2DmgIncrease;
+        
+        if (level == 3)
+        {
+            radius += lv3RadiusIncrease;
+            var shape = ps.shape;
+            shape.radius += lv3RadiusIncrease;
+        }
+
+        if (level == 4)
+            Cooldown.Seconds -= lv4CooldownRed;
     }
 }
